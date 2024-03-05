21208c71dc0b6a3a5bf31a8040a9402ec650a160
Shahid Qureshi <shahid@zeroqe.com>
2024-03-05 23:48:21 +0530

    added pagination in hscode list

--- src/controllers/hscode.controller.js
+++ src/controllers/hscode.controller.js
@@ -1,11 +1,24 @@
 const { HSMapping } = require('../models/database.model');
 
 exports.getAllHsCodes = async (req, res) => {
+  const page = parseInt(req.query.page) || 1; // Current page number, default is 1
+  const limit = parseInt(req.query.limit) || 10; // Number of documents per page, default is 10
+
   try {
-    const list = await HSMapping.find();
-    res.json(list);
+    const totalCount = await HSMapping.countDocuments();
+    const totalPages = Math.ceil(totalCount / limit);
+
+    const list = await HSMapping.find()
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    res.json({
+      data: list,
+      currentPage: page,
+      totalPages: totalPages,
+    });
   } catch (error) {
     console.error(error);
-    res.json({ message: 'Something went wrong', error: error.message });
+    res.status(500).json({ message: 'Something went wrong', error: error.message });
   }
 };
